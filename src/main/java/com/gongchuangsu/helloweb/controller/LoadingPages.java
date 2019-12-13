6dca9eb46431785e9c8a30940a52b7fc8c463e3a
Bestsolution <bestsolution2028@outlook.com>
2019-12-13 18:10:14 +0800

    fix

--- src/main/java/com/gongchuangsu/helloweb/controller/LoadingPages.java
+++ src/main/java/com/gongchuangsu/helloweb/controller/LoadingPages.java
@@ -14,14 +14,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.orm.hibernate4.HibernateTemplate;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.orm.hibernate4.HibernateTemplate;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 @Controller
 public class LoadingPages extends HandlerInterceptorAdapter {
